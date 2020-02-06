bc03d077d2addf59332355525178c82ec9c9c311
Denise Magner <deerenae@gmail.com>
2020-02-06 06:55:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
   {:railroads => 
   {:pieces => 4,
   :rent_in_dollars => {
-    :one_piece_owned => {fee: 25},
+    :one_piece_owned => 25,
     :key2 => {},
     :key3 => {},
     :key4 => {}
